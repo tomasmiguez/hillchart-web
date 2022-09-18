9611a90ca16c1c578ed9fb551802b3313bcf489e
Tomas Miguez <tmiguez@fu.do>
2022-09-18 11:02:22 -0300

    refactor: change then for async/await

--- src/components/Frame.vue
+++ src/components/Frame.vue
@@ -6,35 +6,34 @@ import HillChart from 'hill-chart';
 
 const axios = inject('axios');
 
-onMounted(() => {
-  axios.get('/hillcharts/10')
-    .then((response) => {
-      const frame = response.data.data.frames[0];
-      const scopes = response.data.data.scopes;
-      const data = frame.frameScopes.map((frameScope) =>
-        ({
-          id: frameScope.id,
-          color: scopes.find(scope => scope.id === frameScope.scopeId).color,
-          description: frameScope.title,
-          size: 10,
-          x: frameScope.position,
-        })
-      );
+onMounted(async () => {
+  try {
+    const response = await axios.get('/hillcharts/10');
+    const frame = response.data.data.frames[0];
+    const scopes = response.data.data.scopes;
+    const data = frame.frameScopes.map((frameScope) =>
+      ({
+        id: frameScope.id,
+        color: scopes.find(scope => scope.id === frameScope.scopeId).color,
+        description: frameScope.title,
+        size: 10,
+        x: frameScope.position,
+      })
+    );
 
-      const config = {
-        target: '.hill-chart',
-        width: 700,
-        height: 270,
-        preview: false,
-      };
+    const config = {
+      target: '.hill-chart',
+      width: 700,
+      height: 270,
+      preview: false,
+    };
 
-      const hill = new HillChart(data, config);
+    const hill = new HillChart(data, config);
 
-      hill.render();
-    })
-    .catch((error) => {
-      console.log(error)
-    });
+    hill.render();
+  } catch (error) {
+    console.log(error);
+  }
 });
 </script>
 
