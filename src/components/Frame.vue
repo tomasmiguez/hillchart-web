22cf52f0bed8739675f63c57cf8f61f7f62e20e0
Tomas Miguez <tmiguez@fu.do>
2022-09-17 22:59:14 -0300

    feat: implement hillchart SFC and delete examples

---
+++ src/components/Frame.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { onMounted, inject } from 'vue';
+
+import 'hill-chart/dist/styles.css';
+import HillChart from 'hill-chart';
+
+const axios = inject('axios');
+
+onMounted(() => {
+  axios.get('/hillcharts/10')
+    .then((response) => {
+      const frame = response.data.data.frames[0];
+      const scopes = response.data.data.scopes;
+      const data = frame.frameScopes.map((frameScope) =>
+        ({
+          id: frameScope.id,
+          color: scopes.find(scope => scope.id === frameScope.scopeId).color,
+          description: frameScope.title,
+          size: 10,
+          x: frameScope.position,
+        })
+      );
+
+      const config = {
+        target: '.hill-chart',
+        width: 700,
+        height: 270,
+        preview: false,
+      };
+
+      const hill = new HillChart(data, config);
+
+      hill.render();
+    })
+    .catch((error) => {
+      console.log(error)
+    });
+});
+</script>
+
+<template>
+  <svg class="hill-chart"/>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Hillchart from './components/Hillchart.vue'
+import Frame from './components/Frame.vue'
 
 import './assets/main.css'
 
-const app = createApp(Hillchart);
+import axios from 'axios';
+import applyCaseMiddleware from 'axios-case-converter';
 
-app.config.errorHandler = (err) => {
-  alert(JSON.stringify(err));
-};
+const app = createApp(Frame);
+
+const client = applyCaseMiddleware(axios.create({
+  baseURL: 'http://localhost:3000',
+}));
+app.provide('axios', client);
+
+// app.config.errorHandler = (err) => {
+//   alert(JSON.stringify(err));
+// };
 
 app.mount('#app');
