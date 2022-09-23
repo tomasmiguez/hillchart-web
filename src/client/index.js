7082737edbfa2971630cd4901895aae3a7e913f4
Tomas Miguez <tmiguez@fu.do>
2022-09-22 23:05:57 -0300

    feat: change base url to ngrok

--- src/client/index.js
+++ src/client/index.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import applyCaseMiddleware from 'axios-case-converter';
 
 const client = applyCaseMiddleware(axios.create({
-  baseURL: 'http://localhost:3000',
+  // baseURL: 'http://localhost:3000',
+  baseURL: 'https://1d46-190-190-12-185.ngrok.io',
 }));
 
 export default client;
